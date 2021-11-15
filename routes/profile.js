7411cc687aca05d4ba188f81839f1d327267d97f
Dylan Britz <britzdylan@gmail.com>
2021-11-15 08:36:41 +0200

    profiles done

--- routes/profile.js
+++ routes/profile.js
@@ -40,4 +40,71 @@ router.post(
   })
 );
 
+/***********
+@ Update Profile
+@ Auth: true
+***********/
+router.post(
+  '/update',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const user = await User.findById(req.id);
+
+    if (!user) {
+      res.status(404).send('User does not exist');
+
+      return;
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.id, profiles: { $elemMatch: { _id: req.body.id } } },
+      {
+        $set: {
+          'profiles.$.name': req.body.data.name,
+          'profiles.$.age': req.body.data.age,
+          'profiles.$.avatar': req.body.data.avatar,
+        },
+      },
+      { new: true, timeStamps: false }
+    );
+
+    if (updatedUser instanceof Error) {
+      res.status(500).send(updatedUser);
+      throw new Error(updatedUser);
+    }
+
+    res.status(200).send('Profile Updated successfully');
+  })
+);
+
+/***********
+@ Delete Profile
+@ Auth: true
+***********/
+router.delete(
+  '/delete',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const user = await User.findById(req.id);
+
+    if (!user) {
+      res.status(404).send('User does not exist');
+
+      return;
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.id },
+      { $pull: { profiles: { _id: req.body.id } } },
+      { new: true, timeStamps: false }
+    );
+
+    if (updatedUser instanceof Error) {
+      res.status(500).send(updatedUser);
+      throw new Error(updatedUser);
+    }
+
+    res.status(200).send('Profile Deleted successfully');
+  })
+);
 module.exports = router;
